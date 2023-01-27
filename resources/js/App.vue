bc64ecf6b6cc416f5b546ca52f95e862c0a9c20a
winner00kambale <vainqueurkambale@gmail.com>
2023-01-27 15:39:10 +0200

    innitial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,77 @@
+<template>
+     <div id="wrapper">
+      <!-- sidebar -->
+      <div class="sidebar" :class="{showOverlay: overlayVisibility}">
+        <span class="closeButton" @click="hideOverlay">&times;</span>
+        <p class="brand-title"><a href="">Winner Blog</a></p>
+
+        <div class="side-links">
+          <ul>
+            <li><RouterLink @click="hideOverlay" :to="{name: 'Home'}">Home</RouterLink> </li>
+            <li><RouterLink @click="hideOverlay" :to="{name: 'Blog'}">Blog</RouterLink> </li>
+            <li><RouterLink @click="hideOverlay" :to="{name: 'About'}">About</RouterLink> </li>
+            <li><RouterLink @click="hideOverlay" :to="{name: 'Contact'}">Contact</RouterLink> </li>
+            <li><RouterLink @click="hideOverlay" :to="{name: 'Login'}">Login</RouterLink> </li>
+
+          </ul>
+        </div>
+
+        <!-- sidebar footer -->
+        <footer class="sidebar-footer">
+          <div>
+            <a href=""><i class="fab fa-facebook-f"></i></a>
+            <a href=""><i class="fab fa-instagram"></i></a>
+            <a href=""><i class="fab fa-twitter"></i></a>
+          </div>
+
+          <small>&copy; 2021 Alphayo Blog</small>
+        </footer>
+      </div>
+      <!-- Menu Button -->
+      <div class="menuButton" @click="ShowOverlay">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
+      <!-- main -->
+      <main class="container">
+        <!-- render components depending on the page visited -->
+        <router-view></router-view>
+
+      </main>
+
+      <!-- Main footer -->
+      <footer class="main-footer">
+        <div>
+          <a href=""><i class="fab fa-facebook-f"></i></a>
+          <a href=""><i class="fab fa-instagram"></i></a>
+          <a href=""><i class="fab fa-twitter"></i></a>
+        </div>
+        <small>&copy; 2023 Winner Blog</small>
+      </footer>
+    </div>
+
+</template>
+<script>
+export default {
+    data(){
+        return {
+            overlayVisibility:false,
+        }
+    },
+    methods:{
+        ShowOverlay(){
+            this.overlayVisibility = true
+        },
+        hideOverlay(){
+            this.overlayVisibility = false
+        }
+    }
+}
+</script>
+<style scoped>
+.showOverlay{
+    width: 100%;
+    z-index: 5;
+}
+</style>
---
+++ resources/js/router/index.js
@@ -0,0 +1,49 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import About from '../pages/About.vue';
+import Home from '../pages/Home.vue';
+import Blog from '../pages/Blog.vue';
+import Contact from '../pages/Contact.vue';
+import SingleBlog from '../pages/SingleBlog.vue';
+import Login from '../pages/Login.vue'
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+    },
+    {
+        path: "/blog",
+        name: "Blog",
+        component: Blog,
+    },
+    {
+        path: "/contact",
+        name: "Contact",
+        component: Contact,
+    },
+    {
+        path: "/blog/:slug",
+        name: "SingleBlog",
+        component: SingleBlog,
+        props: true,
+
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+export default router;
